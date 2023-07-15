2a6d8b06384e77af5021c6115ebe7a41d38887c6
kara4600 <kara178@comcast.net>
2023-07-15 12:30:24 -0700

    Update projects page

--- next-app/src/app/projects/page.tsx
+++ next-app/src/app/projects/page.tsx
@@ -5,8 +5,20 @@ import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import List from '@mui/material/List';
+import Image from 'next/image';
+import { WidthFull } from '@mui/icons-material';
 
 const PROJECT_EXPERIENCE = [
+  {
+    subheader: 'Kararizzardi.com',
+    tech: 'Typescript, Next.js, MUI, Docker, Nginx',
+    description:
+      "This is site you're on right now! My personal website uses Typescript with Next.js 13 and MUI. \
+      Docker is used to run the Next.js app and Nginx in separate containers with Docker Compose. \
+      Nginx serves as a reverse-proxy in front of the Next.js app and is also used to cache all the static assets; it passes all requests to the Next.js app's container. \
+      As of now, this project is deployed using an Ubuntu server on a DigitalOcean droplet (though I'm considering other options).",
+    image: '',
+  },
   {
     subheader: 'Scheme Interpreter',
     tech: 'Python',
@@ -15,12 +27,14 @@ const PROJECT_EXPERIENCE = [
       Lexical analysis and syntactical analysis functions parse Scheme tokens into the interpreter's internal Python representation of Scheme expressions. \
       The expression is then evaluated with a mutually recursive eval-apply loop. \
       The __str__ representation of the obtained value is then printed.",
+    image: '/scheme.png',
   },
   {
     subheader: 'Keep Our Library Open Campaign',
     tech: 'HTML, CSS, JavaScript',
     description:
-      'Built a static website to support the community-led movement fighting against the closure of our lcoal library.',
+      'Built a static website to support the community-led movement fighting against the closure of our local library.',
+    image: '/gwc.jpeg',
   },
   {
     subheader: 'Gradehub',
@@ -28,6 +42,18 @@ const PROJECT_EXPERIENCE = [
     description:
       'A Swift project which allows UC Berkeley students to plan their course schedules and calculate their GPA. \
       Firebase is used for user authentication and data management.',
+    image: '/swift-og.png',
+  },
+  {
+    subheader: 'Project SIXT33N',
+    tech: 'NumPy, Jupyter Notebook, Arduino, Machine Learning',
+    description:
+      'SIXT33N is a voice-controlled robot car that can recognize 4 different voice commands. \
+      It can move forward, left or right based on those commands.\
+      It uses a microphone front end circuit integrated with processing integration and PCA classification through an Arduino. \
+      Controls were implemented by gathering open loop parameters and deriving a method to set desired eigenvalues that allow the car to automatically correct itself to drive straight and turn. \
+      This project taught me more about machine learning and hardware (I spent a lot of time debugging circuits).',
+    image: '/SIXT33N.jpg',
   },
 ];
 
@@ -36,7 +62,7 @@ export default function Projects() {
     <Grid
       container
       direction={'column'}
-      width={{ xs: '85vw', sm: '70%' }}
+      width={{ xs: '85%', sm: '70%' }}
       marginX={'auto'}
     >
       <Fade in timeout={1750}>
@@ -48,51 +74,75 @@ export default function Projects() {
           rowSpacing={2}
           marginX={'auto'}
           marginY={{ xs: '50px', sm: '100px' }}
-          width={{ xs: '100%', sm: '55%' }}
+          width={{ xs: '100%', sm: '70%' }}
         >
           <Typography fontWeight={'600'} variant="h4" mb={'2vh'}>
             Projects
           </Typography>
 
           {PROJECT_EXPERIENCE.map(
-            ({ subheader, tech, description }, key) => (
-              <div>
-                <Card
-                  elevation={3}
-                  sx={{
-                    display: 'flex',
-                    padding: '2vh',
-                    borderRadius: 2,
-                    width: '100%',
-                    mb: '4vh',
-                  }}
-                >
-                  <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                    <CardContent>
-                      <List
-                        disablePadding
-                        sx={{
-                          ml: 0,
-                          listStyleType: 'disc',
-                          listStylePosition: 'inside',
-                        }}
+            ({ subheader, tech, description, image }, key) => (
+              <Card
+                elevation={3}
+                sx={{
+                  display: 'flex',
+                  padding: '2vh',
+                  borderRadius: 2,
+                  width: '100%',
+                  mb: '4vh',
+                }}
+              >
+                <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                  <CardContent sx={{ display: { xs: 'none', sm: 'inline' } }}>
+                    {image && (
+                      <Image
+                        src={image}
+                        width={150}
+                        height={150}
+                        alt="Project logo"
+                      />
+                    )}
+                  </CardContent>
+                  <CardContent>
+                    <List
+                      disablePadding
+                      sx={{
+                        ml: 0,
+                        listStyleType: 'disc',
+                        listStylePosition: 'inside',
+                      }}
+                    >
+                      <Typography component="div" fontWeight={600} variant="h5">
+                        {subheader}
+                      </Typography>
+                      <Typography
+                        variant="subtitle1"
+                        color="text.secondary"
+                        component="div"
                       >
-                        <Typography component="div" variant="h5">
-                          {subheader}
-                        </Typography>
-                        <Typography
-                          variant="subtitle1"
-                          color="text.secondary"
-                          component="div"
-                        >
-                          {tech}
-                        </Typography>
-                        <Typography>{description}</Typography>
-                      </List>
+                        {tech}
+                      </Typography>
+                      <Typography>{description}</Typography>
+                    </List>
+                    <CardContent
+                      sx={{
+                        display: { sm: 'none' },
+                        width: 'fit-content',
+                        mx: 'auto',
+                      }}
+                    >
+                      {image && (
+                        <Image
+                          src={image}
+                          width={200}
+                          height={200}
+                          alt="Project logo"
+                        />
+                      )}
                     </CardContent>
-                  </Box>
-                </Card>
-              </div>
+                  </CardContent>
+                </Box>
+              </Card>
             )
           )}
         </Grid>
